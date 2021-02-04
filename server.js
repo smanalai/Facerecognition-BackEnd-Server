75a6fcec95790f6551ec10ee1b8acbf8933c51a4
Sama Manalai <manalais@berea.edu>
2021-02-03 23:08:18 -0500

    fixing bugs in server.js

--- server.js
+++ server.js
@@ -15,46 +15,18 @@ const db =  knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl:true
+    ssl: {
+    rejectUnauthorized: false
+  }
   }
 });
 
-db.select('*').from('users').then(data => {
-	console.log(data);
-})
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-	{
-		id: '123',
-		name: 'John',
-		email: 'john@gmail.com',
-		password: 'cookies',
-		entries: 0,
-		joined: new Date()
 
-	},
-
-	{
-		id: '124',
-		name: 'Sally',
-		email: 'sally@gmail.com',
-		password: 'bananas',
-		entries: 0,
-		joined: new Date()
-	}],
-	login: [
-	{
-		id: '987',
-		hash:'',
-		email: 'john@gmail.com'
-	}
-	]
-}
 
 
 app.get('/', (req, res)=> {
