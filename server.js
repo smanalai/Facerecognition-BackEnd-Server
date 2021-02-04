d32c2d294e79a8349d942a2551f54830b9a96a66
Sama Manalai <manalais@berea.edu>
2021-02-03 22:12:59 -0500

    adding port

--- server.js
+++ server.js
@@ -155,8 +155,8 @@ app.post('/imageurl', (req, res) => {
 })
 
 
-app.listen(3000, ()=> {
-	console.log('App is running on port 3000')
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log('App is running on port $${process.env.PORT}')
 });
 
 
